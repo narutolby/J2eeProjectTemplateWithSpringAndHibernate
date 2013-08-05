2cedccbedb0888edda2ee14afaab2968757dc0f6
liboyang <liboyang@meituan.com>
2013-08-05 20:29:24 +0800

    J2EE Template with Spring ,Spring MVC and Hibernate

---
+++ src/test/java/com/bupt/pcncad/DataSourceFetch.java
@@ -0,0 +1,17 @@
+package com.bupt.pcncad;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: narutolby
+ * Date: 13-8-5
+ * Time: 下午7:34
+ * To change this template use File | Settings | File Templates.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/spring/applicationContext.xml"})
+public class DataSourceFetch {
+}
